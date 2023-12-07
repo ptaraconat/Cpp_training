3b6446cf506de49365f5ddc25550166b0cceeb93
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2023-12-07 07:52:46 +0000

    add my first c++ code

---
+++ sources/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// first C++ script 
+using namespace std ; 
+
+int main()
+{
+    cout << "Hello world" << endl;
+    cout << "this was my first c++ func" << endl;
+    return 0;
+}
